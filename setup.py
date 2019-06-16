99183529340bbab37c05d392e059449603d3a0d0
Kukushkin Alexander <alex@alexkuk.ru>
2019-06-16 17:25:11 +0300

    Up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerus',
-    version='1.2.0',
+    version='1.3.0',
     install_requires=['pullenti_client>=0.2.0'],
     packages=find_packages(),
     entry_points={
