6996bb137e807fcc26bd4d1f593a352c848af0b4
Kukushkin Alexander <alex@alexkuk.ru>
2019-06-16 15:07:57 +0300

    Fix ver

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nerus',
-    version='1.0',
+    version='1.0.0',
     install_requires=[],
     packages=find_packages(),
     entry_points={
