d612acaeff7daedbc63ba9be9ae2d82a105486ee
Kukushkin Alexander <alex@alexkuk.ru>
2020-04-02 22:51:43 +0300

    Up setup tags

--- setup.py
+++ setup.py
@@ -2,13 +2,25 @@
 from setuptools import setup, find_packages
 
 
+with open('README.md') as file:
+    description = file.read()
+
+
 setup(
     name='nerus',
     version='1.4.0',
+    description='Large silver standart Russian corpus with NER, morphology and syntax markup',
+    long_description=description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/natasha/nerus',
+    author='Alexander Kukushkin',
+    author_email='alex@alexkuk.ru',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='russian, nlp, datasets, ner, morphology, syntax',
+    install_requires=[],
     packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'nerus-ctl=nerus.ctl.__main__:main'
-        ],
-    }
 )
