eeef74711043a18f0aeea0ac44b9b3cdd4bc8476
Kukushkin Alexander <alex@alexkuk.ru>
2020-04-04 07:29:01 +0300

    Add nerus api

--- nerus/__init__.py
+++ nerus/__init__.py
@@ -1,2 +1,428 @@
 
-from .load import load_norm as load_nerus  # noqa
+import re
+import gzip
+from itertools import groupby
+
+
+#######
+#
+#  IO
+#
+#######
+
+
+def load_gz_lines(path, encoding='utf8'):
+    with gzip.open(path) as file:
+        for line in file:
+            yield line.decode(encoding).rstrip()
+
+
+######
+#
+#   RECORD
+#
+######
+
+
+class Record(object):
+    __attributes__ = []
+
+    def __init__(self, *args):
+        for key, value in zip(self.__attributes__, args):
+            self.__dict__[key] = value
+
+    def __eq__(self, other):
+        return (
+            type(self) == type(other)
+            and all(
+                (getattr(self, _) == getattr(other, _))
+                for _ in self.__attributes__
+            )
+        )
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __iter__(self):
+        return (getattr(self, _) for _ in self.__attributes__)
+
+    def __hash__(self):
+        return hash(tuple(self))
+
+    def __repr__(self):
+        name = self.__class__.__name__
+        args = ', '.join(
+            '{key}={value!r}'.format(
+                key=_,
+                value=getattr(self, _)
+            )
+            for _ in self.__attributes__
+        )
+        return '{name}({args})'.format(
+            name=name,
+            args=args
+        )
+
+    def _repr_pretty_(self, printer, cycle):
+        name = self.__class__.__name__
+        if cycle:
+            printer.text('{name}(...)'.format(name=name))
+        else:
+            printer.text('{name}('.format(name=name))
+            keys = self.__attributes__
+            size = len(keys)
+            if size:
+                with printer.indent(4):
+                    printer.break_()
+                    for index, key in enumerate(keys):
+                        printer.text(key + '=')
+                        value = getattr(self, key)
+                        printer.pretty(value)
+                        if index < size - 1:
+                            printer.text(',')
+                            printer.break_()
+                printer.break_()
+            printer.text(')')
+
+
+#######
+#
+#   SPAN
+#
+######
+
+
+class Span(Record):
+    __attributes__ = ['start', 'stop', 'type']
+
+    def __init__(self, start, stop, type=None):
+        self.start = start
+        self.stop = stop
+        self.type = type
+
+    def offset(self, delta):
+        return Span(
+            self.start + delta,
+            self.stop + delta,
+            self.type
+        )
+
+
+def offset_spans(spans, delta):
+    for span in spans:
+        yield span.offset(delta)
+
+
+#######
+#
+#   TOKEN
+#
+#######
+
+
+class Token(Record):
+    __attributes__ = ['start', 'stop', 'text']
+
+
+def find_tokens(text, chunks):
+    offset = 0
+    for chunk in chunks:
+        start = text.find(chunk, offset)
+        stop = start + len(chunk)
+        yield Token(start, stop, chunk)
+        offset = stop
+
+
+#######
+#
+#   BIO
+#
+########
+
+
+B, I, O = 'BIO'
+
+
+def parse_bio(tag):
+    if '-' in tag:
+        part, type = tag.split('-', 1)
+    else:
+        part = tag
+        type = None
+    return part, type
+
+
+def bio_spans(tokens, tags):
+    previous = None
+    start = None
+    stop = None
+    for token, tag in zip(tokens, tags):
+        part, type = parse_bio(tag)
+        if part == O:
+            if previous:
+                yield Span(start, stop, previous)
+                previous = None
+        elif part == B:
+            if previous:
+                yield Span(start, stop, previous)
+            previous = type
+            start = token.start
+            stop = token.stop
+        elif part == I:
+            stop = token.stop
+    if previous:
+        yield Span(start, stop, previous)
+
+
+#######
+#
+#  MARKUP
+#
+#####
+
+
+class NERMarkup(Record):
+    __attributes__ = ['text', 'spans']
+
+    def show(self):
+        return show_ner_markup(self)
+
+
+class MorphToken(Record):
+    __attributes__ = ['text', 'pos', 'feats']
+
+
+class MorphMarkup(Record):
+    __attributes__ = ['tokens']
+
+    def show(self):
+        return show_morph_markup(self)
+
+
+class SyntaxToken(Record):
+    __attributes__ = ['id', 'text', 'head_id', 'rel']
+
+
+class SyntaxMarkup(Record):
+    __attributes__ = ['tokens']
+
+    def show(self):
+        return show_syntax_markup(self)
+
+
+def show_ner_markup(markup):
+    try:
+        from ipymarkup import show_span_ascii_markup
+    except ImportError:
+        raise ImportError('install ipymarkup')
+
+    show_span_ascii_markup(markup.text, markup.spans)
+
+
+def format_morph_tag(pos, feats):
+    if not feats:
+        return pos
+
+    feats = '|'.join(
+        '%s=%s' % (_, feats[_])
+        for _ in sorted(feats)
+    )
+    return f'{pos}|{feats}'
+
+
+def format_morph_markup(markup, size=20):
+    words, tags = [], []
+    for token in markup.tokens:
+        words.append(token.text)
+        tags.append(format_morph_tag(token.pos, token.feats))
+
+    for word, tag in zip(words, tags):
+        word = word.rjust(size)
+        yield f'{word}  {tag}'
+
+
+def show_morph_markup(markup):
+    for line in format_morph_markup(markup):
+        print(line)
+
+
+def conll_deps(tokens):
+    for token in tokens:
+        id = int(token.id)
+        head_id = int(token.head_id)
+        rel = token.rel
+        id = id - 1
+        if head_id == 0:  # skip root=0
+            continue
+        head_id = head_id - 1
+        yield head_id, id, rel
+
+
+def show_syntax_markup(markup):
+    try:
+        from ipymarkup import show_dep_ascii_markup
+    except ImportError:
+        raise ImportError('install ipymarkup')
+
+    words = [_.text for _ in markup.tokens]
+    deps = list(conll_deps(markup.tokens))
+    show_dep_ascii_markup(words, deps)
+
+
+#####
+#
+#  NERUS
+#
+########
+
+# # newdoc id = 0
+# # sent_id = 0_0
+# # text = Вице-премьер по социальным вопросам Татьяна Голикова рассказала, ...
+# 1       Вице-премьер    _       NOUN    _       Anma=A   7       nsubj   _       Tag=O
+# 2       по      _       ADP     _       _       4       case    _       Tag=O
+# ...
+
+
+class NerusToken(Record):
+    __attributes__ = ['id', 'text', 'pos', 'feats', 'head_id', 'rel', 'tag']
+
+    @property
+    def morph(self):
+        return MorphToken(self.text, self.pos, self.feats)
+
+    @property
+    def syntax(self):
+        return SyntaxToken(self.id, self.text, self.head_id, self.rel)
+
+
+class NerusSent(Record):
+    __attributes__ = ['id', 'text', 'tokens']
+
+    @property
+    def ner(self):
+        spans = list(sent_spans(self))
+        return NERMarkup(self.text, spans)
+
+    @property
+    def morph(self):
+        tokens = [_.morph for _ in self.tokens]
+        return MorphMarkup(tokens)
+
+    @property
+    def syntax(self):
+        tokens = [_.syntax for _ in self.tokens]
+        return SyntaxMarkup(tokens)
+
+
+class NerusDoc(Record):
+    __attributes__ = ['id', 'sents']
+
+    @property
+    def ner(self):
+        return join_ner_markups(_.ner for _ in self.sents)
+
+
+def sent_spans(sent):
+    words, tags = [], []
+    for token in sent.tokens:
+        words.append(token.text)
+        tags.append(token.tag)
+
+    tokens = find_tokens(sent.text, words)
+    return bio_spans(tokens, tags)
+
+
+def join_ner_markups(markups, sep=' '):
+    texts, spans = [], []
+    offset = 0
+    for markup in markups:
+        texts.append(markup.text)
+        spans.extend(offset_spans(markup.spans, offset))
+        offset += len(markup.text) + len(sep)
+    return NERMarkup(sep.join(texts), spans)
+
+
+def parse_feats(feats):
+    if not feats:
+        return
+
+    for pair in feats.split('|'):
+        key, value = pair.split('=', 1)
+        yield key, value
+
+
+def parse_tag(tag):
+    # Tag=O
+    return tag[4:]
+
+
+def _none(value):
+    if value == '_':
+        return
+    return value
+
+
+def parse_row(line):
+    return [_none(_) for _ in line.split('\t')]
+
+
+def parse_token(line):
+    id, text, _, pos, _, feats, head_id, rel, _, tag = parse_row(line)
+    feats = dict(parse_feats(feats))
+    tag = parse_tag(tag)
+    return NerusToken(id, text, pos, feats, head_id, rel, tag)
+
+
+def group_sents(lines):
+    buffer = []
+    for line in lines:
+        if not line:
+            yield buffer
+            buffer = []
+        else:
+            buffer.append(line)
+    if buffer:
+        yield buffer
+
+
+def parse_attr(line):
+    line = line.lstrip('# ')
+    return line.split(' = ', 1)
+
+
+def parse_sents(lines):
+    for group in group_sents(lines):
+        attrs = {}
+        tokens = []
+        for line in group:
+            if line.startswith('#'):
+                key, value = parse_attr(line)
+                attrs[key] = value
+            else:
+                token = parse_token(line)
+                tokens.append(token)
+
+        id = attrs['sent_id']
+        text = attrs['text']
+
+        yield NerusSent(id, text, tokens)
+
+
+def doc_id(sent):
+    match = re.match(r'^(\d+)_\d+$', sent.id)
+    return match.group(1)
+
+
+def group_docs(sents):
+    for id, group in groupby(sents, key=doc_id):
+        yield NerusDoc(id, list(group))
+
+
+def parse_nerus(lines):
+    sents = parse_sents(lines)
+    return group_docs(sents)
+
+
+def load_nerus(path):
+    lines = load_gz_lines(path)
+    return parse_nerus(lines)
---
+++ nerus/test.py
@@ -0,0 +1,145 @@
+
+from nerus import parse_nerus
+
+
+DATA = '''
+# newdoc id = 0
+# sent_id = 0_0
+# text = Вице-премьер по социальным вопросам Татьяна Голикова рассказала, в каких регионах России зафиксирована наиболее высокая смертность от рака, сообщает РИА Новости.
+1	Вице-премьер	_	NOUN	_	Animacy=Anim|Case=Nom|Gender=Masc|Number=Sing	7	nsubj	_	Tag=O
+2	по	_	ADP	_	_	4	case	_	Tag=O
+3	социальным	_	ADJ	_	Case=Dat|Degree=Pos|Number=Plur	4	amod	_	Tag=O
+4	вопросам	_	NOUN	_	Animacy=Inan|Case=Dat|Gender=Masc|Number=Plur	1	nmod	_	Tag=O
+5	Татьяна	_	PROPN	_	Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing	1	appos	_	Tag=B-PER
+6	Голикова	_	PROPN	_	Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing	5	flat:name	_	Tag=I-PER
+7	рассказала	_	VERB	_	Aspect=Perf|Gender=Fem|Mood=Ind|Number=Sing|Tense=Past|VerbForm=Fin|Voice=Act	0	root	_	Tag=O
+8	,	_	PUNCT	_	_	13	punct	_	Tag=O
+9	в	_	ADP	_	_	11	case	_	Tag=O
+10	каких	_	DET	_	Case=Loc|Number=Plur	11	det	_	Tag=O
+11	регионах	_	NOUN	_	Animacy=Inan|Case=Loc|Gender=Masc|Number=Plur	13	obl	_	Tag=O
+12	России	_	PROPN	_	Animacy=Inan|Case=Gen|Gender=Fem|Number=Sing	11	nmod	_	Tag=B-LOC
+13	зафиксирована	_	VERB	_	Aspect=Perf|Gender=Fem|Number=Sing|Tense=Past|Variant=Short|VerbForm=Part|Voice=Pass	7	ccomp	_	Tag=O
+14	наиболее	_	ADV	_	Degree=Pos	15	advmod	_	Tag=O
+15	высокая	_	ADJ	_	Case=Nom|Degree=Pos|Gender=Fem|Number=Sing	16	amod	_	Tag=O
+16	смертность	_	NOUN	_	Animacy=Inan|Case=Nom|Gender=Fem|Number=Sing	13	nsubj:pass	_	Tag=O
+17	от	_	ADP	_	_	18	case	_	Tag=O
+18	рака	_	NOUN	_	Animacy=Inan|Case=Gen|Gender=Masc|Number=Sing	16	nmod	_	Tag=O
+19	,	_	PUNCT	_	_	20	punct	_	Tag=O
+20	сообщает	_	VERB	_	Aspect=Imp|Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin|Voice=Act	0	root	_	Tag=O
+21	РИА	_	PROPN	_	Animacy=Inan|Case=Nom|Gender=Neut|Number=Sing	20	nsubj	_	Tag=B-ORG
+22	Новости	_	PROPN	_	Animacy=Inan|Case=Nom|Gender=Fem|Number=Plur	21	appos	_	Tag=I-ORG
+23	.	_	PUNCT	_	_	20	punct	_	Tag=O
+
+# sent_id = 0_1
+# text = По словам Голиковой, чаще всего онкологические заболевания становились причиной смерти в Псковской, Тверской, Тульской и Орловской областях, а также в Севастополе.
+1	По	_	ADP	_	_	2	case	_	Tag=O
+2	словам	_	NOUN	_	Animacy=Inan|Case=Dat|Gender=Neut|Number=Plur	9	parataxis	_	Tag=O
+3	Голиковой	_	PROPN	_	Animacy=Anim|Case=Gen|Gender=Fem|Number=Sing	2	nmod	_	Tag=B-PER
+4	,	_	PUNCT	_	_	2	punct	_	Tag=O
+5	чаще	_	ADV	_	Degree=Cmp	9	advmod	_	Tag=O
+6	всего	_	PRON	_	Animacy=Inan|Case=Gen|Gender=Neut|Number=Sing	5	obl	_	Tag=O
+7	онкологические	_	ADJ	_	Case=Nom|Degree=Pos|Number=Plur	8	amod	_	Tag=O
+8	заболевания	_	NOUN	_	Animacy=Inan|Case=Nom|Gender=Neut|Number=Plur	9	nsubj	_	Tag=O
+9	становились	_	VERB	_	Aspect=Imp|Mood=Ind|Number=Plur|Tense=Past|VerbForm=Fin|Voice=Mid	0	root	_	Tag=O
+10	причиной	_	NOUN	_	Animacy=Inan|Case=Ins|Gender=Fem|Number=Sing	9	xcomp	_	Tag=O
+11	смерти	_	NOUN	_	Animacy=Inan|Case=Gen|Gender=Fem|Number=Sing	10	nmod	_	Tag=O
+12	в	_	ADP	_	_	20	case	_	Tag=O
+13	Псковской	_	ADJ	_	Case=Loc|Degree=Pos|Gender=Fem|Number=Sing	20	amod	_	Tag=B-LOC
+14	,	_	PUNCT	_	_	15	punct	_	Tag=O
+15	Тверской	_	ADJ	_	Case=Loc|Degree=Pos|Gender=Fem|Number=Sing	13	conj	_	Tag=B-LOC
+16	,	_	PUNCT	_	_	17	punct	_	Tag=O
+17	Тульской	_	ADJ	_	Case=Loc|Degree=Pos|Gender=Fem|Number=Sing	13	conj	_	Tag=B-LOC
+18	и	_	CCONJ	_	_	19	cc	_	Tag=O
+19	Орловской	_	ADJ	_	Case=Loc|Degree=Pos|Gender=Fem|Number=Sing	13	conj	_	Tag=B-LOC
+20	областях	_	NOUN	_	Animacy=Inan|Case=Loc|Gender=Fem|Number=Plur	9	obl	_	Tag=I-LOC
+21	,	_	PUNCT	_	_	25	punct	_	Tag=O
+22	а	_	CCONJ	_	_	25	cc	_	Tag=O
+23	также	_	ADV	_	Degree=Pos	22	fixed	_	Tag=O
+24	в	_	ADP	_	_	25	case	_	Tag=O
+25	Севастополе	_	PROPN	_	Animacy=Inan|Case=Loc|Gender=Masc|Number=Sing	20	conj	_	Tag=B-LOC
+26	.	_	PUNCT	_	_	9	punct	_	Tag=O
+'''.strip().splitlines()
+
+REPR = '''NerusDoc(id='0', sents=[NerusSent(id='0_0', text='Вице-премьер по социальным вопросам Татьяна Голикова рассказала, в каких регионах России зафиксирована наиболее высокая смертность от рака, сообщает РИА Новости.', tokens=[NerusToken(id='1', text='Вице-премьер', pos='NOUN', feats={'Animacy': 'Anim', 'Case': 'Nom', 'Gender': 'Masc', 'Number': 'Sing'}, head_id='7', rel='nsubj', tag='O')'''
+
+
+NER = '''
+Вице-премьер по социальным вопросам Татьяна Голикова рассказала, в 
+                                    PER─────────────               
+каких регионах России зафиксирована наиболее высокая смертность от 
+               LOC───                                              
+рака, сообщает РИА Новости. По словам Голиковой, чаще всего 
+               ORG────────            PER──────             
+онкологические заболевания становились причиной смерти в Псковской, 
+                                                         LOC──────  
+Тверской, Тульской и Орловской областях, а также в Севастополе.
+LOC─────  LOC─────   LOC───────────────            LOC──────── 
+'''[1:]
+
+MORPH = '''
+        Вице-премьер  NOUN|Animacy=Anim|Case=Nom|Gender=Masc|Number=Sing
+                  по  ADP
+          социальным  ADJ|Case=Dat|Degree=Pos|Number=Plur
+            вопросам  NOUN|Animacy=Inan|Case=Dat|Gender=Masc|Number=Plur
+             Татьяна  PROPN|Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing
+            Голикова  PROPN|Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing
+          рассказала  VERB|Aspect=Perf|Gender=Fem|Mood=Ind|Number=Sing|Tense=Past|VerbForm=Fin|Voice=Act
+                   ,  PUNCT
+                   в  ADP
+               каких  DET|Case=Loc|Number=Plur
+            регионах  NOUN|Animacy=Inan|Case=Loc|Gender=Masc|Number=Plur
+              России  PROPN|Animacy=Inan|Case=Gen|Gender=Fem|Number=Sing
+       зафиксирована  VERB|Aspect=Perf|Gender=Fem|Number=Sing|Tense=Past|Variant=Short|VerbForm=Part|Voice=Pass
+            наиболее  ADV|Degree=Pos
+             высокая  ADJ|Case=Nom|Degree=Pos|Gender=Fem|Number=Sing
+          смертность  NOUN|Animacy=Inan|Case=Nom|Gender=Fem|Number=Sing
+                  от  ADP
+                рака  NOUN|Animacy=Inan|Case=Gen|Gender=Masc|Number=Sing
+                   ,  PUNCT
+            сообщает  VERB|Aspect=Imp|Mood=Ind|Number=Sing|Person=3|Tense=Pres|VerbForm=Fin|Voice=Act
+                 РИА  PROPN|Animacy=Inan|Case=Nom|Gender=Neut|Number=Sing
+             Новости  PROPN|Animacy=Inan|Case=Nom|Gender=Fem|Number=Plur
+                   .  PUNCT
+'''[1:]
+
+SYNTAX = '''
+  ┌►┌─┌───── Вице-премьер  nsubj
+  │ │ │ ┌──► по            case
+  │ │ │ │ ┌► социальным    amod
+  │ │ └►└─└─ вопросам      nmod
+  │ └────►┌─ Татьяна       appos
+  │       └► Голикова      flat:name
+┌─└───────── рассказала    
+│   ┌──────► ,             punct
+│   │   ┌──► в             case
+│   │   │ ┌► каких         det
+│   │ ┌►└─└─ регионах      obl
+│   │ │ └──► России        nmod
+└──►└─└───── зафиксирована ccomp
+    │     ┌► наиболее      advmod
+    │   ┌►└─ высокая       amod
+    └►┌─└─── смертность    nsubj:pass
+      │   ┌► от            case
+      └──►└─ рака          nmod
+          ┌► ,             punct
+      ┌─┌─└─ сообщает      
+      │ └►┌─ РИА           nsubj
+      │   └► Новости       appos
+      └────► .             punct
+'''[1:]
+
+
+def test(capsys):
+    doc = next(parse_nerus(DATA))
+
+    assert repr(doc).startswith(REPR)
+
+    doc.ner.show()
+    assert capsys.readouterr().out == NER
+
+    sent = doc.sents[0]
+    sent.morph.show()
+    assert capsys.readouterr().out == MORPH
+
+    sent.syntax.show()
+    assert capsys.readouterr().out == SYNTAX
