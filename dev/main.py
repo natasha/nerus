5f5af17b81aa98edd300369ba2960ec7c3ab4194
Kukushkin Alexander <alex@alexkuk.ru>
2020-04-04 07:28:29 +0300

    Add merge

--- dev/main.py
+++ dev/main.py
@@ -3,8 +3,11 @@ from os.path import (
     exists, expanduser,
     join as join_path
 )
-from collections import defaultdict
-from itertools import islice as head
+from collections import defaultdict, OrderedDict
+from itertools import (
+    islice as head,
+    groupby,
+)
 import json
 import gzip
 
@@ -35,11 +38,13 @@ LENTA_TOTAL = 739351
 NER = join_path(DATA_DIR, 'ner.jl.gz')
 MORPH = join_path(DATA_DIR, 'morph.jl.gz')
 SYNTAX = join_path(DATA_DIR, 'syntax.jl.gz')
+NERUS = join_path(DATA_DIR, 'nerus_lenta.conllu.gz')
 
 S3_LENTA = LENTA
 S3_NER = NER
 S3_MORPH = MORPH
 S3_SYNTAX = SYNTAX
+S3_NERUS = NERUS
 
 #######
 #  PROCESS
@@ -49,9 +54,17 @@ HOST = 'localhost'
 NER_PORT = 8080
 MORPH_PORT = 8081
 SYNTAX_PORT = 8082
-CHUNK_SIZE = 10000
-# ~5/100k > 128
-SENT_LEN = 128
+NER_CHUNK_SIZE = 10000
+MORPH_CHUNK_SIZE = 100000
+SYNTAX_CHUNK_SIZE = 1000000
+
+#####
+#  BIO
+#######
+
+B = 'B'
+I = 'I'
+O = 'O'
 
 ######
 #  CONFIG
@@ -115,8 +128,27 @@ def parse_jl(lines):
 #######
 
 
+def parse_annotation(annotation):
+    type = annotation or str
+
+    repeatable = False
+    if isinstance(annotation, list):  # [Fact]
+        repeatable = True
+        type = annotation[0]
+
+    is_record = issubclass(type, Record)
+
+    return type, repeatable, is_record
+
+
 class Record(object):
     __attributes__ = []
+    __annotations__ = {}
+
+    def __init__(self, *args, **kwargs):
+        for key, value in zip(self.__attributes__, args):
+            self.__dict__[key] = value
+        self.__dict__.update(kwargs)
 
     def __eq__(self, other):
         return (
@@ -170,6 +202,42 @@ class Record(object):
                             printer.break_()
                 printer.break_()
             printer.text(')')
+
+    @property
+    def as_json(self):
+        data = OrderedDict()
+        for key in self.__attributes__:
+            annotation = self.__annotations__.get(key)
+            _, repeatable, is_record = parse_annotation(annotation)
+
+            value = getattr(self, key)
+            if value is None:
+                continue
+
+            if repeatable and is_record:
+                value = [_.as_json for _ in value]
+            elif is_record:
+                value = value.as_json
+
+            data[key] = value
+        return data
+
+    @classmethod
+    def from_json(cls, data):
+        args = []
+        for key in cls.__attributes__:
+            annotation = cls.__annotations__.get(key)
+            type, repeatable, is_record = parse_annotation(annotation)
+            value = data.get(key)
+            if value is None and repeatable:
+                value = []
+            elif value is not None:
+                if repeatable and is_record:
+                    value = [type.from_json(_) for _ in value]
+                elif is_record:
+                    value = type.from_json(value)
+            args.append(value)
+        return cls(*args)
 
 
 ########
@@ -230,13 +298,20 @@ def chop(items, size):
 #####
 
 
+class ProcessError(Exception):
+    pass
+
+
 def process_call(data, host, port):
     url = f'http://{host}:{port}'
     response = requests.post(
         url,
         json=data
     )
-    response.raise_for_status()
+    try:
+        response.raise_for_status()
+    except:
+        raise ProcessError
     return response.json()
 
 
@@ -251,7 +326,7 @@ def ner_chunks(records, chunk_size):
         yield [_.text for _ in chunk]
 
 
-def process_ner(records, host=HOST, port=NER_PORT, chunk_size=CHUNK_SIZE):
+def process_ner(records, host=HOST, port=NER_PORT, chunk_size=NER_CHUNK_SIZE):
     for chunk in ner_chunks(records, chunk_size):
         data = process_call(chunk, host, port)
         yield from data
@@ -262,23 +337,21 @@ def process_ner(records, host=HOST, port=NER_PORT, chunk_size=CHUNK_SIZE):
 ########
 
 
-def morph_items(records, sent_len):
+def morph_items(records):
     for record in records:
         sents = sentenize(record.text)
         for sent in sents:
             tokens = tokenize(sent.text)
-            tokens = head(tokens, sent_len)
             yield [_.text for _ in tokens]
 
 
-def morph_chunks(records, chunk_size, sent_len):
-    items = morph_items(records, sent_len)
+def morph_chunks(records, chunk_size):
+    items = morph_items(records)
     yield from chop(items, chunk_size)
 
 
-def process_morph(records, host=HOST, port=MORPH_PORT,
-                  chunk_size=CHUNK_SIZE, sent_len=SENT_LEN):
-    for chunk in morph_chunks(records, chunk_size, sent_len):
+def process_morph(records, host=HOST, port=MORPH_PORT, chunk_size=MORPH_CHUNK_SIZE):
+    for chunk in morph_chunks(records, chunk_size):
         data = process_call(chunk, host, port)
         yield from data
 
@@ -316,14 +389,277 @@ def reorder(items, order):
 
 def process_size(groups, host, port):
     for group in groups:
-        data = process_call(group, host, port)
-        yield from data
+        try:
+            data = process_call(group, host, port)
+        except ProcessError:
+            # rare cases of groups of too long sents (~0.007% 172/2496485)
+            # concat trimed masks
+            for _ in group:
+                yield {}
+        else:
+            yield from data
 
 
-def process_syntax(records, host=HOST, port=SYNTAX_PORT,
-                   chunk_size=CHUNK_SIZE, sent_len=SENT_LEN):
-    for chunk in syntax_chunks(records, chunk_size, sent_len):
+def process_syntax(records, host=HOST, port=SYNTAX_PORT, chunk_size=SYNTAX_CHUNK_SIZE):
+    for chunk in syntax_chunks(records, chunk_size):
         order, groups = order_size(chunk)
         data = process_size(groups, host, port)
         yield from reorder(data, order)
 
+
+#######
+#
+#   SPAN
+#
+#######
+
+
+class Span(Record):
+    __attributes__ = ['start', 'stop', 'type']
+
+    def __init__(self, start, stop, type=None):
+        self.start = start
+        self.stop = stop
+        self.type = type
+
+    def offset(self, delta):
+        return Span(
+            self.start + delta,
+            self.stop + delta,
+            self.type
+        )
+
+
+def offset_spans(spans, delta):
+    for span in spans:
+        yield span.offset(delta)
+
+
+def envelop_span(envelope, span):
+    return envelope.start <= span.start and span.stop <= envelope.stop
+
+
+def envelop_spans(envelope, spans):
+    for span in spans:
+        if envelop_span(envelope, span):
+            yield span
+
+
+########
+#
+#   MARKUP
+#
+##########
+
+
+class NERMarkup(Record):
+    __attributes__ = ['text', 'spans']
+    __annotations__ = {
+        'spans': [Span]
+    }
+
+
+class MorphToken(Record):
+    __attributes__ = ['text', 'pos', 'feats']
+
+
+class MorphMarkup(Record):
+    __attributes__ = ['tokens']
+    __annotations__ = {
+        'tokens': [MorphToken]
+    }
+
+
+class SyntaxToken(Record):
+    __attributes__ = ['id', 'text', 'head_id', 'rel']
+
+
+class SyntaxMarkup(Record):
+    __attributes__ = ['tokens']
+    __annotations__ = {
+        'tokens': [SyntaxToken]
+    }
+
+
+parse_ner_markup = NERMarkup.from_json
+parse_morph_markup = MorphMarkup.from_json
+parse_syntax_markup = SyntaxMarkup.from_json
+
+
+#######
+#
+#   BIO
+#
+######
+
+
+def format_bio(part, type):
+    if not type:
+        return part
+    return '%s-%s' % (part, type)
+
+
+def append_ellipsis(items, ellipsis=None):
+    for item in items:
+        yield item
+    yield ellipsis
+
+
+def spans_bio(tokens, spans):
+    spans = append_ellipsis(spans)
+    span = next(spans)
+    for token in tokens:
+        part = O
+        type = None
+        if span:
+            if token.start >= span.start:
+                type = span.type
+                if token.start == span.start:
+                    part = B
+                else:
+                    part = I
+            if token.stop >= span.stop:
+                span = next(spans)
+        yield format_bio(part, type)
+
+
+#######
+#
+#   TOKEN
+#
+########
+
+
+class Token(Record):
+    __attributes__ = ['start', 'stop', 'text']
+
+
+def find_tokens(text, chunks):
+    offset = 0
+    for chunk in chunks:
+        start = text.find(chunk, offset)
+        stop = start + len(chunk)
+        yield Token(start, stop, chunk)
+        offset = stop
+
+
+########
+#
+#   MERGE
+#
+#######
+
+
+class MergeToken(Record):
+    __attributes__ = ['id', 'text', 'pos', 'feats', 'head_id', 'rel', 'tag']
+
+
+class MergeRecord(Record):
+    __attributes__ = ['doc_id', 'sent_index', 'text', 'spans', 'tokens']
+
+
+def sent_spans(sent, spans):
+    spans = envelop_spans(sent, spans)
+    return offset_spans(spans, -sent.start)    
+
+
+def markup_words(markup):
+    for token in markup.tokens:
+        yield token.text
+
+
+def merge_tokens(morphs, syntaxes, tags):
+    for morph, syntax, tag in zip(morphs, syntaxes, tags):
+        yield MergeToken(
+            syntax.id, morph.text,
+            morph.pos, morph.feats,
+            syntax.head_id, syntax.rel,
+            tag
+        )
+
+
+def find_next(sent, markups):
+    for markup in markups:
+        if not markup.tokens:
+            return markup
+
+        token = markup.tokens[0]
+        if sent.text.startswith(token.text):
+            return markup
+
+
+def merge(docs, ners, morphs, syntaxes):
+    for doc_id, doc in enumerate(docs):
+        ner = next(ners)
+        sents = sentenize(doc.text)
+        for sent_index, sent in enumerate(sents):
+            morph = find_next(sent, morphs)
+            syntax = find_next(sent, syntaxes)
+            if not morph or not syntax:
+                break
+
+            if len(morph.tokens) != len(syntax.tokens):
+                continue
+
+            spans = list(sent_spans(sent, ner.spans))
+            words = markup_words(morph)
+            tokens = find_tokens(sent.text, words)
+            tags = spans_bio(tokens, spans)
+
+            tokens = list(merge_tokens(morph.tokens, syntax.tokens, tags))
+            yield MergeRecord(
+                doc_id, sent_index,
+                sent.text, spans, tokens
+            )
+
+
+######
+#
+#   CONLL
+#
+######
+
+
+def format_feats(feats):
+    if not feats:
+        return '_'
+
+    return '|'.join(
+        '%s=%s' % (_, feats[_])
+        for _ in sorted(feats)
+    )
+
+
+def format_merge_conll(records):
+    # https://universaldependencies.org/format.html
+
+    # ID, FORM, LEMMA,
+    # UPOS, XPOS, FEATS
+    # HEAD, DEPREL, DEPS
+    # MISC    
+
+    # # newdoc id = mf920901-001
+    # # newpar id = mf920901-001-p1
+    # # sent_id = mf920901-001-p1s1A
+    # # text = Slovenská ústava: pro i proti
+    # # text_en = Slovak constitution: pros and cons
+    # 1   Slovenská   slovenský   ADJ     AAFS1----1A---- Case=Nom|Degree=Pos|...
+    # 2   ústava      ústava      NOUN    NNFS1-----A---- Case=Nom|Gender=Fem|...
+    # 3   :           :           PUNCT   Z:------------- _          2       p...
+    # 4   pro         pro         ADP     RR--4---------- Case=Acc   2       a...
+
+    for doc_id, group in groupby(records, key=lambda _: _.doc_id):
+        yield f'# newdoc id = {doc_id}'
+        for record in group:
+            sent_id = f'{record.doc_id}_{record.sent_index}'
+            yield f'# sent_id = {sent_id}'
+            yield f'# text = {record.text}'
+            for token in record.tokens:
+                feats = format_feats(token.feats)
+                yield (
+                    f'{token.id}\t{token.text}\t_'  # no lemma
+                    f'\t{token.pos}\t_\t{feats}'  # just pos, no xpos
+                    f'\t{token.head_id}\t{token.rel}\t_'  # just deprel, no deps
+                    f'\tTag={token.tag}'  # misc = ner tag
+                )
+            yield ''
